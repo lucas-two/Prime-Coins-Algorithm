f23ec0eeafcf8407697101780b233766b6391e84
lucas-two <43457360+lucas-two@users.noreply.github.com>
2019-04-12 15:08:02 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,78 @@
+"""
+Prime-Coin Problem v0.1 12/04/19
+
+GOALS:
+- Implement a checker to see if duplicate coin sets are
+ being used e.g. (2, 3) and (3, 2) are the same.
+
+- Implement a way to calculate a set of prime numbers
+for our coins. We are currently doing this statically.
+
+- Implement a maximum/range of coins we want to be used
+ for a given amount.
+
+ - Make our function behave recursively.
+"""
+
+import time
+
+
+def prime_coins(amount, coins):
+    """
+    Using BFS, performs backtracking to find combinations of
+    coins to equal a certain amount.
+    :param amount: A given amount we will find coins to equal.
+    :param coins: Our selection of coins to use
+    :return: A list of solutions
+    """
+    solutions = []
+    visited = []
+    queue = []
+
+    # Append initial nodes to the queue
+    for node in coins:
+        queue.append([node])
+
+    # Do this while we have elements in the queue
+    while queue:
+        node = queue.pop(0)  # Take off the first node
+        visited.append(node)  # Add it to the visited queue
+
+        # Calculate the total of your current node.
+        current_amount = calc_total(node)
+
+        # If we've met the exact amount, return this as a solution
+        if current_amount == amount:
+            solutions.append(node)
+            print("SOLUTION: %s" % node)
+
+        # Otherwise, look at the node's children (ONLY if it's total is less than the current amount)
+        elif current_amount < amount:
+            for coin in coins:
+                child = node[:]
+                child.append(coin)
+                queue.append(child)
+
+    return solutions
+
+
+def calc_total(node):
+    """
+    Calculates the total cost of a given node.
+    :param node: Our current coins we have (node)
+    :return: The sum total of all coins it's coins
+    """
+    total = 0
+    for coin in node:
+        total += coin
+    return total
+
+
+amount_test = 5
+coins_test = [1, 2, 3, 5] # NOTE: 1 is included as well as gold coin (5)
+
+start_time = time.time()
+solution = prime_coins(amount_test, coins_test)
+end_time = time.time()
+
+print("%s solutions found in %.6f secs" % (len(solution), end_time - start_time))
